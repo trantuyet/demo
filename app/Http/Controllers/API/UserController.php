b1dcf562515d5b1ae782a3370eefa2d5ac7bef0b
ducc1998 <duccoc98@gmail.com>
2021-01-20 14:49:45 +0700

    edit

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -3,9 +3,16 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    //
+    function edit($id , Request $request){
+        $user = User::find($id);
+        $user->fill($request ->all());
+        $user->save();
+        return response()->json($user,200);
+
+    }
 }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class UserSeeder extends Seeder
 {
@@ -14,7 +15,10 @@ class UserSeeder extends Seeder
      */
     public function run()
     {
-        User::factory(10)
-            -
+        $user =new User();
+        $user->name = 'Duc';
+        $user->email = 'duc@gmail.com';
+        $user->password = Hash::make('12345678');
+        $user->save();
     }
 }
